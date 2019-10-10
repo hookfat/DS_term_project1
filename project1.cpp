1d9de8d36e8b6d4bee59884ec73ab6515ca25495
hookfat <seven05287017@gmail.com>
2019-10-10 17:48:14 +0800

    完成讀入方塊的部分

--- project1.cpp
+++ project1.cpp
@@ -23,8 +23,10 @@ public:
     Listnode* prev;
     bool* data;
 };
-
-
+typedef struct{
+    int row;
+    int col;
+}block;
 
 int main()
 {
@@ -35,19 +37,254 @@ int main()
     int row, col;
     char c[5];
     in >> row >> col;
-    Listnode* head = Listnode(col);
+    Listnode* head = new Listnode(col);
     Listnode* temp = head;
-    
+
     for(int i = 0; i<row+3; i++){
-        temp->next = Listnode(col);
+        temp->next = new Listnode(col);
         temp->next->prev = temp;
         temp = temp->next;
     }
-    
+
     in.getline(c, 1);
     while(!in.eof()){
         in.getline(c, 1003);
-        
+        //READ
+        block down_block[4]; //用來記錄讀入的block的 row,col
+        int bottom_block_num;  //用來記錄有幾個block在底部
+        if(c[0] == 'T'){
+            int down_col = c[3] - '0' -1;
+            if(c[1] == '1'){
+                down_block[0].row = -2;
+                down_block[0].col = down_col;
+                down_block[1].row = -2;
+                down_block[1].col = down_col+2;
+                down_block[2].row = -1;
+                down_block[2].col = down_col+1;
+                down_block[3].row = -2;
+                down_block[3].col = down_col+1;
+                bottom_block_num = 3;
+            }
+            else if(c[1] == '2'){
+                down_block[0].row = -2;
+                down_block[0].col = down_col;
+                down_block[1].row = -1;
+                down_block[1].col = down_col+1;
+                down_block[2].row = -2;
+                down_block[2].col = down_col+1;
+                down_block[3].row = -3;
+                down_block[3].col = down_col+1;
+                bottom_block_num = 2;
+            }
+            else if(c[1] == '3'){
+                down_block[0].row = -1;
+                down_block[0].col = down_col;
+                down_block[1].row = -1;
+                down_block[1].col = down_col+1;
+                down_block[2].row = -1;
+                down_block[2].col = down_col+2;
+                down_block[3].row = -2;
+                down_block[3].col = down_col+1;
+                bottom_block_num = 3;
+            }
+            else if(c[1] == '4'){
+                down_block[0].row = -2;
+                down_block[0].col = down_col+1;
+                down_block[1].row = -1;
+                down_block[1].col = down_col;
+                down_block[2].row = -2;
+                down_block[2].col = down_col;
+                down_block[3].row = -3;
+                down_block[3].col = down_col;
+                bottom_block_num = 2;
+            }
+        }
+        else if(c[0] == 'L'){
+            int down_col = c[3] - '0' -1;
+            if(c[1] == '1'){
+                down_block[0].row = -1;
+                down_block[0].col = down_col;
+                down_block[1].row = -1;
+                down_block[1].col = down_col+1;
+                down_block[2].row = -2;
+                down_block[2].col = down_col;
+                down_block[3].row = -2;
+                down_block[3].col = down_col;
+                bottom_block_num = 2;
+            }
+            else if(c[1] == '2'){
+                down_block[0].row = -2;
+                down_block[0].col = down_col+1;
+                down_block[1].row = -2;
+                down_block[1].col = down_col+2;
+                down_block[2].row = -1;
+                down_block[2].col = down_col;
+                down_block[3].row = -2;
+                down_block[3].col = down_col;
+                bottom_block_num = 3;
+            }
+            else if(c[1] == '3'){
+                down_block[0].row = -3;
+                down_block[0].col = down_col;
+                down_block[1].row = -1;
+                down_block[1].col = down_col+1;
+                down_block[2].row = -2;
+                down_block[2].col = down_col+1;
+                down_block[3].row = -3;
+                down_block[3].col = down_col+1;
+                bottom_block_num = 2;
+            }
+            else if(c[1] == '4'){
+                down_block[0].row = -1;
+                down_block[0].col = down_col;
+                down_block[1].row = -1;
+                down_block[1].col = down_col+1;
+                down_block[2].row = -1;
+                down_block[2].col = down_col+2;
+                down_block[3].row = -2;
+                down_block[3].col = down_col+2;
+                bottom_block_num = 3;
+            }
+        }
+        else if(c[0] == 'J'){
+            int down_col = c[3] - '0' -1;
+            if(c[1] == '1'){
+                down_block[0].row = -1;
+                down_block[0].col = down_col;
+                down_block[1].row = -1;
+                down_block[1].col = down_col+1;
+                down_block[2].row = -2;
+                down_block[2].col = down_col+1;
+                down_block[3].row = -3;
+                down_block[3].col = down_col+1;
+                bottom_block_num = 2;
+            }
+            else if(c[1] == '2'){
+                down_block[0].row = -1;
+                down_block[0].col = down_col;
+                down_block[1].row = -1;
+                down_block[1].col = down_col+1;
+                down_block[2].row = -1;
+                down_block[2].col = down_col+2;
+                down_block[3].row = -2;
+                down_block[3].col = down_col;
+                bottom_block_num = 3;
+            }
+            else if(c[1] == '3'){
+                down_block[0].row = -3;
+                down_block[0].col = down_col+1;
+                down_block[1].row = -1;
+                down_block[1].col = down_col;
+                down_block[2].row = -2;
+                down_block[2].col = down_col;
+                down_block[3].row = -3;
+                down_block[3].col = down_col;
+                bottom_block_num = 2;
+            }
+            else if(c[1] == '4'){
+                down_block[0].row = -2;
+                down_block[0].col = down_col;
+                down_block[1].row = -2;
+                down_block[1].col = down_col+1;
+                down_block[2].row = -1;
+                down_block[2].col = down_col+2;
+                down_block[3].row = -2;
+                down_block[3].col = down_col+2;
+                bottom_block_num = 3;
+            }
+        }
+        else if(c[0] == 'S'){
+            int down_col = c[3] - '0' -1;
+            if(c[1] == '1'){
+                down_block[0].row = -2;
+                down_block[0].col = down_col+2;
+                down_block[1].row = -1;
+                down_block[1].col = down_col;
+                down_block[2].row = -1;
+                down_block[2].col = down_col+1;
+                down_block[3].row = -2;
+                down_block[3].col = down_col+1;
+                bottom_block_num = 3;
+            }
+            else if(c[1] == '2'){
+                down_block[0].row = -2;
+                down_block[0].col = down_col;
+                down_block[1].row = -1;
+                down_block[1].col = down_col+1;
+                down_block[2].row = -2;
+                down_block[2].col = down_col+1;
+                down_block[3].row = -3;
+                down_block[3].col = down_col;
+                bottom_block_num = 2;
+            }
+        }
+        else if(c[0] == 'Z'){
+            int down_col = c[3] - '0' -1;
+            if(c[1] == '1'){
+                down_block[0].row = -2;
+                down_block[0].col = down_col;
+                down_block[1].row = -1;
+                down_block[1].col = down_col+1;
+                down_block[2].row = -1;
+                down_block[2].col = down_col+2;
+                down_block[3].row = -2;
+                down_block[3].col = down_col+1;
+                bottom_block_num = 3;
+            }
+            else if(c[1] == '2'){
+                down_block[0].row = -2;
+                down_block[0].col = down_col+1;
+                down_block[1].row = -1;
+                down_block[1].col = down_col;
+                down_block[2].row = -2;
+                down_block[2].col = down_col;
+                down_block[3].row = -3;
+                down_block[3].col = down_col+1;
+                bottom_block_num = 2;
+            }
+        }
+        else if(c[0] == 'I'){
+            int down_col = c[3] - '0' -1;
+            if(c[1] == '1'){
+                down_block[0].row = -1;
+                down_block[0].col = down_col;
+                down_block[1].row = -2;
+                down_block[1].col = down_col;
+                down_block[2].row = -3;
+                down_block[2].col = down_col;
+                down_block[3].row = -4;
+                down_block[3].col = down_col;
+                bottom_block_num = 1;
+            }
+            else if(c[1] == '2'){
+                down_block[0].row = -1;
+                down_block[0].col = down_col;
+                down_block[1].row = -1;
+                down_block[1].col = down_col+1;
+                down_block[2].row = -1;
+                down_block[2].col = down_col+2;
+                down_block[3].row = -1;
+                down_block[3].col = down_col+3;
+                bottom_block_num = 4;
+            }
+        }
+        else if(c[0] == 'O'){
+                int down_col = c[3] - '0' -1;
+                down_block[0].row = -1;
+                down_block[0].col = down_col;
+                down_block[1].row = -1;
+                down_block[1].col = down_col+1;
+                down_block[2].row = -2;
+                down_block[2].col = down_col;
+                down_block[3].row = -2;
+                down_block[3].col = down_col+1;
+                bottom_block_num = 2;
+        }
+        else if(c[0] == 'E'){
+            break;
+        }
+        //READ
+
 
     }
 
